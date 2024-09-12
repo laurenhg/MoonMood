4656e64200400cf687c08f0ee334ea6988ee7704
laurenhg <laurenhg@gmail.com>
2024-09-12 15:21:04 +0200

    menu setup

---
+++ main.py
@@ -0,0 +1,8 @@
+import moon_phase
+def print_menu():
+    """Display the menu options to the user."""
+    print("Moon Phase Application Menu:")
+    print("1. Get Current Moon Phase")
+    print("2. Get Advice Based on Current Moon Phase")
+    print("3. Look Up Upcoming Phases")
+    print("4. Exit")
